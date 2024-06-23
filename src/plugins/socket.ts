28efc1b7a4ade29fd5eb80f60e3fbb7c0cfde8a9
karanhudia <karanhudia@grover.com>
2024-06-23 16:23:16 +0530

    chore: handle socket try catch

--- src/plugins/socket.ts
+++ src/plugins/socket.ts
@@ -2,21 +2,13 @@ import { FastifyPluginAsync } from 'fastify';
 import fastifyIO from 'fastify-socket.io';
 import { Lottie } from '../models/lottie';
 import { Server } from 'socket.io';
+import { updateLottieColorProperty } from '../utils/lottie';
+import { CreateLottieMessage, LottieSocketEvents, UpdateLottieMessage } from '../graphql/generated';
 
 declare module 'fastify' {
   interface FastifyInstance {
     io: Server;
   }
-}
-
-type LottieJSONPayload = {
-  uuid: string;
-  json: JSON;
-};
-
-enum LottieJSONSocketEvent {
-  CreateJSON = 'createJSON',
-  UpdateJSON = 'updateJSON',
 }
 
 export const fastifySocketIo: FastifyPluginAsync = async (fastify) => {
@@ -25,33 +17,104 @@ export const fastifySocketIo: FastifyPluginAsync = async (fastify) => {
   fastify.io.on('connection', (socket) => {
     fastify.log.info('Socket connected!', socket.id);
 
-    socket.on(LottieJSONSocketEvent.CreateJSON, async (message: LottieJSONPayload) => {
-      fastify.log.info('Creating new Editable JSON lottie');
+    socket.on(LottieSocketEvents.CreateJson, async (message: CreateLottieMessage) => {
+      fastify.log.info('Creating:: JSON lottie');
 
-      await Lottie.create({
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        ...message,
-      });
+      try {
+        await Lottie.create({
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          uuid: message.uuid,
+          json: message.payload?.json,
+        });
 
-      fastify.log.info('Added new JSON lottie');
+        fastify.log.info('Created:: JSON lottie');
+
+        return {
+          code: 200,
+          status: 'ok',
+        };
+      } catch {
+        fastify.log.error('Error:: JSON lottie');
+
+        return {
+          code: 500,
+          status: 'Could not create new JSON',
+        };
+      }
     });
 
-    socket.on(LottieJSONSocketEvent.UpdateJSON, async (message: LottieJSONPayload) => {
-      fastify.log.info('Updating existing UUID', message.uuid);
+    socket.on(
+      LottieSocketEvents.UpdateJson,
+      async (message: UpdateLottieMessage, acknowledgement) => {
+        fastify.log.info(`Updating(${message.uuid}):: JSON lottie`);
 
-      await Lottie.updateOne(
-        { uuid: message.uuid },
-        {
-          $set: {
-            updatedAt: new Date(),
-            ...message,
-          },
-        },
-      );
+        const foundLottie = await Lottie.findOne({ uuid: message.uuid });
 
-      fastify.log.info('Updated JSON lottie with UUID: ', message.uuid);
-    });
+        if (!foundLottie) {
+          fastify.log.error(`Updating(${message.uuid}):: JSON lottie not found`);
+
+          return {
+            code: 404,
+            status: 'Could not find JSON',
+          };
+        }
+
+        try {
+          let updatedLottie = undefined;
+
+          switch (message.payload.__typename) {
+            case 'ColorPayload':
+              updatedLottie = updateLottieColorProperty(
+                foundLottie.json,
+                message.payload.layer,
+                message.payload.shape,
+                message.payload.shapeItem,
+                message.payload.color,
+              );
+
+              fastify.log.info(`Updating(${message.uuid}):: JSON lottie color`);
+              break;
+
+            case 'SpeedPayload':
+              updatedLottie = {
+                ...foundLottie.json,
+                fr: message.payload.frameRate,
+              };
+
+              fastify.log.info(`Updating(${message.uuid}):: JSON lottie speed`);
+              break;
+          }
+
+          await Lottie.updateOne(
+            { uuid: message.uuid },
+            {
+              $set: {
+                updatedAt: new Date(),
+                json: updatedLottie,
+              },
+            },
+          );
+
+          // Broadcast this change to every client except the sender
+          socket.broadcast.emit(LottieSocketEvents.UpdateJson, message);
+
+          fastify.log.info(`Updating(${message.uuid}):: JSON lottie successfully updated`);
+
+          acknowledgement({
+            code: 200,
+            status: 'ok',
+          });
+        } catch {
+          fastify.log.error(`Updating(${message.uuid}):: Internal Server Error`);
+
+          acknowledgement({
+            code: 500,
+            status: 'Could not update JSON',
+          });
+        }
+      },
+    );
   });
 
   fastify.addHook('onReady', () => {
