98960d3e5280e0da8739016f9d9c79be9f9b30a6
karanhudia <karanhudia@grover.com>
2024-06-23 16:16:03 +0530

    feat: add codegen

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const port = process.env.PORT || 4000;
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: `http://localhost:${port}/graphql`,
+  generates: {
+    'src/generated/graphql.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
+    },
+  },
+  hooks: { afterAllFileWrite: ['prettier --write'] },
+};
+
+export default config;
